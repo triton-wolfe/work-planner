4b1ade89efe15296de8e75cd3227b629f8a18d16
Triton Wolfe <twolfe@scriptpro.com>
2018-07-06 15:59:13 -0500

    try #4

---
+++ sources/controllers/DashBoardCtrl.js
@@ -0,0 +1,8 @@
+(function () {
+    angular.module('work-planner')
+        .controller('DashBoardCtrl', ['$state', '$rootScope', '$scope', DashBoardCtrl]);
+
+    function DashBoardCtrl($state, $rootScope, $scope) {
+
+    }
+})
