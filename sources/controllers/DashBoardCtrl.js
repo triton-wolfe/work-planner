510934b59db44a799a4651495ed419068e18d643
Triton Wolfe <triton.wolfe@gmail.com>
2018-07-08 16:27:34 -0500

    try #7

--- sources/controllers/DashBoardCtrl.js
+++ sources/controllers/DashBoardCtrl.js
@@ -1,5 +1,5 @@
 (function () {
-    angular.module('work-planner')
+    angular.module('work-planner', [])
     .controller('DashBoardCtrl', ['$scope', function($scope){
         $scope.field = 'hi';
     }]);
