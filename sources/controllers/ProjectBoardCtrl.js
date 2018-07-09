35ab4dbf46da033cbd6ca3bc4343609a978cbdf1
Triton Wolfe <triton.wolfe@gmail.com>
2018-07-09 18:49:51 -0500

    try #1

--- sources/controllers/ProjectBoardCtrl.js
+++ sources/controllers/ProjectBoardCtrl.js
@@ -4,8 +4,8 @@
 
     function ProjectBoardCtrl($scope, firebase) {
         var projectsRef = firebase.database().ref().child('projects');
-        var thisCtrl = this;
         this.projects = [];
+        var thisCtrl = this;
 
         projectsRef.on('value', function(snap) {
             value = snap.val();
