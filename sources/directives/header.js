1e46f8a0b18420853adc53b70a760db4694446e7
Triton Wolfe <triton.wolfe@gmail.com>
2018-07-09 21:14:05 -0500

    add header

---
+++ sources/directives/header.js
@@ -0,0 +1,32 @@
+angular.module('work-planner')
+    .directive('header', function () {
+        return {
+            restrict: "A",
+            controller: ['$scope', '$state' function($scope, $state) {
+                $scope.goToDashBoard = function () {
+                    $state.go('DashBoard');
+                };
+                $scope.goToProjects = function () {
+                    $state.go('ProjectBoard');
+                };
+                $scope.goToCalender = function () {
+                    $state.go('Calendar');
+                };
+            }],
+            template: `<nav class="navbar navbar-default navbar-fixed-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <img class="navbar-brand" ng-click="vm.goToDashBoard()"
+                            src="work-planner/sources/images/logo.jpg"/>
+                    </div>
+                    <div id="navbar" class="navbar-collapse collapse">
+                      <ul class="nav navbar-nav">
+                        <li><span ng-click="goToDashBoard()">DashBoard</span></li>
+                        <li><span ng-click="goToCalender()">Calender</span></li>
+                        <li><span ng-click="goToProjects()">Projects</span></li>
+                      </ul>
+                    </div>
+                </div>
+            </nav>`
+        }
+    });
