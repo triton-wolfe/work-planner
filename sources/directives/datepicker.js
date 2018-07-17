f741827a343c0b941bd9b418e85dfbdcad1a8db2
Triton Wolfe <triton.wolfe@gmail.com>
2018-07-17 16:22:19 -0500

    attempt #3

--- sources/directives/datepicker.js
+++ sources/directives/datepicker.js
@@ -1,16 +1,16 @@
 angular.module('work-planner')
-directive('datetimepicker', ['$timeout',
-    function ($timeout) {
-        return {
-            require: 'model',
-            restrict: 'AE',
-            scope: {
-                model: '='
-            },
-            link: function (scope, elem, attrs, ctrl) {
-                elem.datetimepicker()
-            },
-            template: `<input form="text" ng-model="model"></input>`
-        };
-    }
-]);
+    .directive('datetimepicker', ['$timeout',
+        function ($timeout) {
+            return {
+                require: 'model',
+                restrict: 'AE',
+                scope: {
+                    model: '='
+                },
+                link: function (scope, elem, attrs, ctrl) {
+                    elem.datetimepicker()
+                },
+                template: `<input form="text" ng-model="model"></input>`
+            };
+        }
+    ]);
