46abcc0319b89a3b339b1a6959c363ad3607db9e
Triton Wolfe <triton.wolfe@gmail.com>
2018-07-17 11:47:00 -0500

    attempt with jquery

--- sources/directives/datepicker.js
+++ sources/directives/datepicker.js
@@ -1,28 +1,16 @@
 angular().module('work-planner')
-    .directive('datepicker', ['$compile', function ($compile) {
+directive('datetimepicker', ['$timeout',
+    function ($timeout) {
         return {
-            restrict: 'E',
+            require: 'model',
+            restrict: 'AE',
             scope: {
                 model: '='
             },
-            link: function(scope, element) {
-                scope.format = "MM/dd/yyyy";
-                scope.isOpen = false;
-                scope.datePickerOpen = function ($event) {
-
-                }
-
+            link: function (scope, elem, attrs, ctrl) {
+                elem.datetimepicker()
             },
-            template: `
-            <input id="datepicker-{{question.questionId}}"
-                       class="form-control form-control-sm date"
-                       type="text"
-                       placeholder="mm/dd/ccyy"
-                       datepicker-popup="{{format}}"
-                       ng-model="model"
-                       is-open="isOpen"
-                       ng-click="datePickerOpen($event)"
-                       close-text="Close"
-                       datepicker-options="{showWeeks: false, initDate: inDate}"></input>`
-        }
-    }]);
+            template: `<input form="text" ng-model="model"></input>`
+        };
+    }
+]);
